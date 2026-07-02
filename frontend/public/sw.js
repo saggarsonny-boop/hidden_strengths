abea32611992eababd466c34d8249cab2cde1727
Sonny Saggar <saggarsonny@gmail.com>
2026-07-01 19:46:12 -0500

    Step 2 – Add Service Worker

---
+++ frontend/public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'jobmatch-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
